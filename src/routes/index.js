880cfe3a388c0d1cba575c193b6d39a209d1f01d
HungNg1012 <hung.nguyen1012@hcmut.edu.vn>
2022-11-09 11:57:13 +0700

    Create History page by HungNg

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,19 @@
 // Import Layout
 
-
 // Import Route Pages
-import HomePage from "../pages/Home"
+import HomePage from "../pages/Home";
+import History from "../pages/History";
 
 // Not Required Login
 // layout:null --> No Layout
 const publicRoutes = [
-    {path: "/", component: HomePage },
-    {path: "/sample", component: HomePage},
-    {path: "/sample/setting", component: HomePage}
-]
+  { path: "/", component: HomePage },
+  { path: "/sample", component: HomePage },
+  { path: "/sample/setting", component: HomePage },
+  { path: "/history", component: History },
+];
 
-const privateRoutes = []
+const privateRoutes = [];
 
-export {publicRoutes,privateRoutes}
+export { publicRoutes, privateRoutes };
 //Require Login
