f11f2c23a465b20e7e4335ef5dbc0adf4219610e
tri218138 <80521604+tri218138@users.noreply.github.com>
2022-11-09 21:44:07 +0700

    Add files via upload

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,66 +1,44 @@
-import styles from './mainpage.module.css'
-// import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
-// import mapboxgl from '!mapbox-gl';
-import React, { useRef, useEffect, useState } from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactMapboxGl from "react-mapbox-gl";
+import DrawControl from "react-mapbox-gl-draw";
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import MapboxDraw from "@mapbox/mapbox-gl-draw";
-// npm install @mapbox/mapbox-gl-draw
-// https://github.com/mapbox/mapbox-gl-draw
+import '@mapbox/mapbox-gl-supported';
 import mapboxgl from 'mapbox-gl';
-// https://docs.mapbox.com/mapbox-gl-js/guides/install/ ----------guide
+// import "./styles.css";
+// npm cache clean --force
+// document: https://github.com/amaurym/react-mapbox-gl-draw
+// polygon https://codesandbox.io/s/mapbox-polygon-forked-qcu4om?file=/src/index.js
 
+const Map = ReactMapboxGl({
+    accessToken:
+    'pk.eyJ1Ijoibmh0aHVuZzEwMTIiLCJhIjoiY2w5NWEzbHczMmJlbjNucGMydGhnNHNheCJ9.CaiZuHejM4TIVmh4KnMpaw'
+});
+const onDrawCreate = ({ features }) => {
+    console.log(features);
+    let k = features[0].geometry.coordinates;
+    console.log(k[0]);
+};
 
-function HomePage() {
-    const mapContainer = useRef(0);
-    const map = useRef(0);
-    const [lng, setLng] = useState(-91.874);
-    const [lat, setLat] = useState(42.76);
-    const [zoom, setZoom] = useState(10);
-    mapboxgl.accessToken = 'pk.eyJ1Ijoibmh0aHVuZzEwMTIiLCJhIjoiY2w5NWEzbHczMmJlbjNucGMydGhnNHNheCJ9.CaiZuHejM4TIVmh4KnMpaw';
-    useEffect(() => {
-        if (map.current) return; // initialize map only once
-            map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [lng, lat],
-            zoom: zoom
-        });
-    });
-    
-    useEffect(() => {
-        if (!map.current) return; // wait for map to initialize
-            map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
-            setLat(map.current.getCenter().lat.toFixed(4));
-            setZoom(map.current.getZoom().toFixed(2));            
-        });
-    });
-    // var Draw = new MapboxDraw();
+const onDrawUpdate = ({ features }) => {
+    console.log(features);
+};
 
-    // // map.addControl(Draw, 'top-left');
-    // map.on('draw.create', updateArea);
-    // map.on('draw.delete', updateArea);
-    // map.on('draw.update', updateArea);
-    // function updateArea(e) {
-    //     const data = Draw.getAll();
-    //     // const answer = document.getElementById('calculated-area');
-    //     if (data.features.length > 0) {
-    //     // const area = turf.area(data);
-    //     // Restrict the area to 2 decimal points.
-    //     // const rounded_area = Math.round(area * 100) / 100;
-    //     // answer.innerHTML = `<p><strong>${rounded_area}</strong></p><p>square meters</p>`;
-    //     } else {
-    //     // answer.innerHTML = '';
-    //     if (e.type !== 'draw.delete')
-    //     alert('Click the map to draw a polygon.');
-    //     }
-    // }
+function HomePage() {  
     return (
         <div>
-            {/* <div className={styles.sidebar}>
-            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-            </div> */}
-            <div ref={mapContainer} className={styles.mapContainer} />
+            <Map 
+                style="mapbox://styles/mapbox/satellite-v9" // eslint-disable-line
+                center={[-91.874, 42.76]}
+                containerStyle={{
+                    height: "600px",
+                    // width: "100vw"
+                }}
+                zoom={[12]}
+            >
+                <DrawControl onDrawCreate={onDrawCreate} onDrawUpdate={onDrawUpdate}/>
+            </Map>
         </div>
     );
 }
