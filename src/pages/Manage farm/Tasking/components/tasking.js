d1538b64088a23db16c689454405cf5beb09df30
NguyenHuuDanh <113421911+DanhNguyen02@users.noreply.github.com>
2022-11-10 14:59:22 +0700

    Update by Danh

--- src/pages/Manage farm/Tasking/components/tasking.js
+++ src/pages/Manage farm/Tasking/components/tasking.js
@@ -1,34 +1,30 @@
 import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import { Icon } from '@iconify/react'
 import './tasking.scss'
+import data from './data.json'
 
 function Tasking() {
+    const { register, getValues } = useForm();
     const [openAddingTask, setOpenAddingTask] = useState(false)
-    const addingTask = () => {
+    const switchToAddTask = () => {
+        setOpenAddingTask(!openAddingTask)
+        document.getElementsByTagName("form")[0].reset()
+    }
+    const addTask = () => {
+        alert(JSON.stringify(getValues()));
         setOpenAddingTask(!openAddingTask)
     }
-    const tasks = [
-        {
-            name: "Công việc 1", 
-            time:{day: 0, hour: 0, minute: 57}
-        },
-        {
-            name: "Công việc 2", 
-            time:{day: 0, hour: 2, minute: 34}
-        },
-        {
-            name: "Công việc 3", 
-            time:{day: 0, hour: 3, minute: 16}
-        },
-        {
-            name: "Công việc 4", 
-            time:{day: 0, hour: 10, minute: 2}
-        }
-    ];
+    const delTask = () => {
+        setOpenAddingTask(!openAddingTask)
+    } 
+    const editTask = () => {
+        setOpenAddingTask(!openAddingTask)
+    }
 
     function convertTime(time) {
-        let string_time = ""
-        if (time.day != 0) string_time += time.day + "d ";
+        let string_time = "";
+        if (time.day !== 0) string_time += time.day + "d ";
         string_time += time.hour + "h " + time.minute + "m";
         return string_time
     }
@@ -44,7 +40,7 @@ function Tasking() {
                 <div className="option">
                     <Icon icon="bx:dots-horizontal-rounded" className="option-icon" />
                     <div className="option-content">
-                        <div><button onClick={addingTask}>Chỉnh sửa</button></div>
+                        <div><button onClick={editTask}>Chỉnh sửa</button></div>
                         <div><button>Xóa</button></div>
                     </div>
                 </div>
@@ -57,11 +53,11 @@ function Tasking() {
                 <div className='manage-task'>
                     <div className='show-task-list'>
                         <div className='add'>
-                            <button type='button' className="add-button" onClick={addingTask}>Tạo công việc</button>
+                            <button type='button' className="add-button" onClick={switchToAddTask}>Tạo công việc</button>
                         </div>
                         <div className='task-list'>
                             <ul>
-                                {tasks.map((task) => <Task name={task.name} time={task.time} />)}
+                                {data.map((data) => <Task name={data['name']} time={data['time']} />)}
                             </ul>
                         </div>
                         <div className='numbering'>
@@ -73,38 +69,40 @@ function Tasking() {
                     </div>
                 </div>
                 <div className='add-task'>
-                    <div className='section'>
-                        <input type='text' placeholder="Đặt tên chủ đề"></input>
-                    </div>
-                    <div className='description'>
-                        <div className='leftside'>
-                            <textarea placeholder='Nội dung...'></textarea>
+                    <form>
+                        <div className='section'>
+                            <input type='text' placeholder="Đặt tên chủ đề" {...register("section")}></input>
                         </div>
-                        <div className='rightside'>
-                            <div>
-                                <label for='date'>Chọn ngày:</label>
-                                <input type='date' name='date'></input>
-                                <div className='set-time'>
-                                    <label for='start'>Từ:</label>
-                                    <input type='time' name='start'></input>
-                                    <label for='end'>Đến:</label>
-                                    <input type='time' name='end'></input>
-                                </div>
+                        <div className='description'>
+                            <div className='leftside'>
+                                <textarea placeholder='Nội dung...' {...register("content")}></textarea>
+                            </div>
+                            <div className='rightside'>
                                 <div>
-                                    <select>
-                                        <option>Không lặp</option>
-                                        <option>Hằng ngày</option>
-                                        <option>Hằng tuần</option>
-                                        <option>Hằng tháng</option>
-                                    </select>
-                                </div>
-                                <div className='add-task-button'>
-                                    <button type='button' className='del-task' onClick={addingTask}>Xóa</button>
-                                    <button type='button' className='adding-task' onClick={addingTask}>Thêm công việc</button>
+                                    <label for='date'>Chọn ngày:</label>
+                                    <input type='date' name='date'></input>
+                                    <div className='set-time'>
+                                        <label for='start'>Từ:</label>
+                                        <input type='time' name='start'></input>
+                                        <label for='end'>Đến:</label>
+                                        <input type='time' name='end'></input>
+                                    </div>
+                                    <div>
+                                        <select>
+                                            <option default>Không lặp</option>
+                                            <option>Hằng ngày</option>
+                                            <option>Hằng tuần</option>
+                                            <option>Hằng tháng</option>
+                                        </select>
+                                    </div>
+                                    <div className='add-task-button'>
+                                        <button type='button' className='del-task' onClick={delTask}>Xóa</button>                                        
+                                        <button type='button' className='adding-task' onClick={addTask}>Thêm công việc</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
