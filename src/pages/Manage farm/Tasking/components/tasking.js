784cafc6c86c5a2f01b707b42ebcaf46b79eb76f
NguyenHuuDanh <113421911+DanhNguyen02@users.noreply.github.com>
2022-11-14 14:27:06 +0700

    New updates

--- src/pages/Manage farm/Tasking/components/tasking.js
+++ src/pages/Manage farm/Tasking/components/tasking.js
@@ -5,30 +5,80 @@ import './tasking.scss'
 import data from './data.json'
 
 function Tasking() {
-    const { register, getValues } = useForm();
+    // task data
+    const tasks = [
+        {
+            id: 1,
+            name: "Công việc 1",
+            description: "Là công việc đấy", 
+            time: {"day": 0, "hour": 0, "minute": 57}
+        },
+        {
+            id: 2,
+            name: "Công việc 2",
+            description: "Về nhà trồng rau nuôi cá",  
+            time: {"day": 0, "hour": 2, "minute": 34}
+        },
+        {
+            id: 3,
+            name: "Công việc 3", 
+            description: "Mang tiền về cho mẹ", 
+            time: {"day": 0, "hour": 3, "minute": 16}
+        },
+        {
+            id: 4,
+            name: "Công việc 4",
+            description: "Đừng mang ưu phiền về cho mẹ",  
+            time: {"day": 1, "hour": 10, "minute": 2}
+        }
+    ]
+    const [taskList, updateTaskList] = useState(tasks)
+    const { register, getValues, setValue } = useForm();
     const [openAddingTask, setOpenAddingTask] = useState(false)
     const [addOrEditTask, setAddOrEditTask] = useState(true)
+    const [idOfEditedTask, setIdOfEditedTask] = useState()
     const switchToAddTask = () => {
         setAddOrEditTask(true)
         setOpenAddingTask(!openAddingTask)
         document.getElementsByTagName("form")[0].reset()
     }
-    const switchToEditTask = () => {
+    const switchToEditTask = (e) => {
         setAddOrEditTask(false)
+        let id = e.target.getAttribute("id"), 
+            name = e.target.getAttribute('name'), 
+            description = e.target.getAttribute('description')
+        setValue('name', name)
+        setValue('description', description)
+        setIdOfEditedTask(id)
         setOpenAddingTask(!openAddingTask)
     }
-    const addTask = () => {
-        alert(JSON.stringify(getValues()));
+    const handleAddTask = () => {
+        let newTask = {id:taskList.length+1, name: getValues().section, time: {"day": 1, "hour": 10, "minute": 2}}
+        let newTaskList = taskList.concat(newTask)
+        updateTaskList(newTaskList)
         setOpenAddingTask(!openAddingTask)
     }
-    const removeTask = () => {
-        setOpenAddingTask(!openAddingTask)
+    const handleRemoveTask = (e) => {
+        let id = e.target.getAttribute("id")
+        updateTaskList(taskList.filter(task => task.id != id))
     }
+    const denyAddTask = () => {
+        setOpenAddingTask(!openAddingTask)
+    } 
     const denyChange = () => {
         setOpenAddingTask(!openAddingTask)
     }
-    const saveEditTask = () => {
-        alert(JSON.stringify(getValues()));
+    const handleEditTask = () => {
+        const newTaskList = taskList.map((task) => {
+            if (task.id == idOfEditedTask) {
+                const updatedTask = task
+                updatedTask.name = getValues().name
+                updatedTask.description = getValues().description
+                return updatedTask
+            }
+            return task
+        })
+        updateTaskList(newTaskList)
         setOpenAddingTask(!openAddingTask)
     } 
 
@@ -42,16 +92,27 @@ function Tasking() {
     function Task(props) {
         return (
             <li>
-                <h2>{props.name}</h2>
+                <h2>{props.taskInfo.name}</h2>
                 <Icon icon="ic:baseline-access-time" className="time-icon" />
                 <div className='time'>
-                    <p>{convertTime(props.time)}</p>
+                    <p>{convertTime(props.taskInfo.time)}</p>
                 </div>
                 <div className="option">
                     <Icon icon="bx:dots-horizontal-rounded" className="option-icon" />
                     <div className="option-content">
-                        <div><button onClick={switchToEditTask}>Chỉnh sửa</button></div>
-                        <div><button>Xóa</button></div>
+                        <div>
+                            <button 
+                                id={props.taskInfo.id}
+                                name={props.taskInfo.name}
+                                description={props.taskInfo.description}
+                                onClick={switchToEditTask}
+                            >Chỉnh sửa</button></div>
+                        <div>
+                            <button 
+                                id={props.taskInfo.id}
+                                onClick={handleRemoveTask}
+                            >Xóa</button>
+                        </div>
                     </div>
                 </div>
             </li>
@@ -71,7 +132,7 @@ function Tasking() {
                         </div>
                         <div className='task-list'>
                             <ul>
-                                {data.map((data) => <Task name={data['name']} time={data['time']} />)}
+                                {taskList.map((task) => <Task taskInfo={task} />)}
                             </ul>
                         </div>
                         <div className='numbering'>
@@ -95,13 +156,13 @@ function Tasking() {
                         <div className='section'>
                             <input 
                                 type='text' 
-                                placeholder="Đặt tên chủ đề" {...register("section")}
+                                placeholder="Đặt tên chủ đề" {...register("name")}
                             ></input>
                         </div>
                         <div className='description'>
                             <div className='leftside'>
                                 <textarea 
-                                    placeholder='Nội dung...' {...register("content")}
+                                    placeholder='Nội dung...' {...register("description")}
                                 ></textarea>
                             </div>
                             <div className='rightside'>
@@ -124,12 +185,12 @@ function Tasking() {
                                     </div>
                                     <div id={addOrEditTask ? "add" : "edit"}>
                                         <div className='add-task-buttons'>
-                                            <button type='button' className='del-task' onClick={removeTask}>Xóa</button>                                        
-                                            <button type='button' className='adding-task' onClick={addTask}>Thêm công việc</button>
+                                            <button type='button' className='del-task' onClick={denyAddTask}>Xóa</button>                                        
+                                            <button type='button' className='adding-task' onClick={handleAddTask}>Thêm công việc</button>
                                         </div>
                                         <div className='edit-task-buttons'>
                                             <button type='button' className='del-change' onClick={denyChange}>Bỏ thay đổi</button>                                        
-                                            <button type='button' className='save-edit-task' onClick={saveEditTask}>Lưu chỉnh sửa</button>
+                                            <button type='button' className='save-edit-task' onClick={handleEditTask}>Lưu chỉnh sửa</button>
                                         </div>
                                     </div>
                                 </div>
