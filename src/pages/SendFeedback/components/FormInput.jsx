848157487e0ecbec9d4b23d823d139e230da340a
HungPham-Canh <113783361+HungPham-Canh@users.noreply.github.com>
2022-11-27 22:34:17 +0700

    Update FormInput.jsx

--- src/pages/SendFeedback/components/FormInput.jsx
+++ src/pages/SendFeedback/components/FormInput.jsx
@@ -3,20 +3,39 @@ import "./FormInput.css";
 
 const FormInput = (props) => {
   const [focused, setFocused] = useState(false);
-  const { label, onChange, ...inputProps } = props;
+  const [error, setError] = useState(false);
+  const {id, label, submit, setSubmit, errorMessage, maxLength, onChange, ...inputProps } = props;
   const handleFocus = () => {
     setFocused(true);
   };
+
   return (
     <div className="FormInput">
       <label>{label}</label>
       <textarea
         {...inputProps}
-        onChange={onChange}
+        onChange={(e) => {
+            const newSubmit = [...submit];
+            
+            if (e.target.value.length <= maxLength) {
+              setError(false);
+              newSubmit[id - 1] = true;
+            }
+            else {
+              setError(true);
+              newSubmit[id - 1] = false;
+            }
+
+            setSubmit(newSubmit);
+            onChange(e);
+          }
+        }
         onBlur={handleFocus}
         onFocus={() => inputProps.name === "content" && setFocused(true)}
         focused={focused.toString()}
+        error={error.toString()}
       />
+     <span>{errorMessage}</span>
     </div>
   );
 };
