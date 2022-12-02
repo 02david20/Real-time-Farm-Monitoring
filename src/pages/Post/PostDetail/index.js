94ceaf33d7d3c47089782d6414e8c37f76bfb8e8
HungNg1012 <hung.nguyen1012@hcmut.edu.vn>
2022-12-02 18:36:05 +0700

    Done post cmt func

--- src/pages/Post/PostDetail/index.js
+++ src/pages/Post/PostDetail/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faHeart, faUser } from "@fortawesome/free-regular-svg-icons";
 import { faHeart as heart } from "@fortawesome/free-solid-svg-icons";
 import {
   faFacebookF,
@@ -18,8 +18,47 @@ const post = {
 
 function PostDetail() {
   const [toggle, setToggle] = useState(false);
+  const [comments, setComments] = useState([
+    {
+      name: "User A",
+      time: "2/11/2022 15:25:43",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      name: "User B",
+      time: "21/11/2022 10:12:43",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      name: "User C",
+      time: "26/11/2022 22:51:43",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+  ]);
+
   function handleClick() {
     setToggle(!toggle);
+  }
+
+  function upCmt() {
+    let content = document.getElementById("comment_text").value;
+    console.log(content);
+    const dt = new Date();
+    const padL = (nr, len = 2, chr = `0`) => `${nr}`.padStart(2, chr);
+
+    let time = `${padL(dt.getMonth() + 1)}/${padL(
+      dt.getDate()
+    )}/${dt.getFullYear()} ${padL(dt.getHours())}:${padL(
+      dt.getMinutes()
+    )}:${padL(dt.getSeconds())}`;
+
+    let newCmt = comments.unshift({
+      name: "User",
+      time: time,
+      content: content,
+    });
+
+    setComments((comments) => [...comments, newCmt]);
   }
 
   return (
@@ -62,6 +101,81 @@ function PostDetail() {
             <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
           </button>
         </div>
+      </Row>
+      <Row className="d-flex justify-content-center">
+        <Col md="12" lg="10" xl="8">
+          <div className="card">
+            <div
+              className="card-header py-3 border-0"
+              style={{ backgroundColor: "#f8f9fa" }}
+            >
+              <div className="d-flex flex-start w-100">
+                <div className="form-outline w-100">
+                  <textarea
+                    className="form-control"
+                    id="comment_text"
+                    rows="4"
+                    style={{ background: "#fff", resize: "none" }}
+                    placeholder="Bình luận mới"
+                  ></textarea>
+                </div>
+              </div>
+              <div className="float-end mt-2 pt-1">
+                <button
+                  form="cmt"
+                  className="btn-orange btn btn-primary btn-sm"
+                  value="commentin"
+                  onClick={function (e) {
+                    upCmt();
+                  }}
+                >
+                  Đăng
+                </button>
+                <input
+                  type="reset"
+                  className="btn-orange-out btn btn-outline-primary btn-sm ms-3"
+                  value="Hủy"
+                ></input>
+              </div>
+            </div>
+
+            {comments.map((comment) => (
+              <div
+                className="card-body"
+                key={comment.name}
+                style={{ borderBottom: "1px solid grey" }}
+              >
+                <div className="d-flex flex-start align-items-center">
+                  <div>
+                    <h6 className="fw-bold text-primary mb-1">
+                      {comment.name}
+                    </h6>
+                    <p className="text-muted small mb-0">{comment.time}</p>
+                  </div>
+                </div>
+                <p className="mt-3 mb-4 pb-2">{comment.content}</p>
+                <div className="small d-flex justify-content-start">
+                  <a className="d-flex align-items-center me-3">
+                    <i className="far fa-thumbs-up me-2"></i>
+                    <p className="mb-0">Thích</p>
+                  </a>
+                  <a className="d-flex align-items-center me-3">
+                    <i className="far fa-comment-dots me-2"></i>
+                    <p className="mb-0">Bình luận</p>
+                  </a>
+                  <a href="" className="d-flex align-items-center me-3">
+                    <i className="fas fa-share me-2"></i>
+                    <p className="mb-0">Chia sẻ</p>
+                  </a>
+                  <a href="" className="d-flex align-items-center me-3">
+                    <i className="fas fa-exclamation-triangle me-2"></i>
+                    <p className="mb-0">Xóa</p>
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </Col>
       </Row>
     </div>
   );
